e7151a1bd57af07e2a8e13dff7735b4607da43a2
duvanjm <duvanjarin@gmail.com>
2021-07-19 12:03:39 -0500

    add task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes a string str1 and a string str2 as
+    arguments and returns a concatenated string"""
+    return str1 + str2
