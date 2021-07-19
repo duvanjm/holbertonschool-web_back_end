86c9d83df12cb6c22a136ea84e24ae623200f581
duvanjm <duvanjarin@gmail.com>
2021-07-19 12:04:13 -0500

    add task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def to_str(n: float) -> str:
+    """takes a float n as argument and
+    returns the string representation of the float."""
+    return str(n)
