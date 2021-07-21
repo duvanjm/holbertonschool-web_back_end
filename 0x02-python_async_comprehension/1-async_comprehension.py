1dee21ad262926c84a27a2f35889db6f3d738a4b
duvanjm <duvanjarin@gmail.com>
2021-07-21 13:48:24 -0500

    fix type return

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """module"""
 
-from typing import List
+from typing import Generator
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Generator[float, None, None]:
     """collect 10 random numbers using an async
     comprehensing over async_generator"""
     lst = []
