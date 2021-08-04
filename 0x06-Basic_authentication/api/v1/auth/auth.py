e260f6abeae6ac25d63b4d1aae5ebda2f235698e
duvanjm <duvanjarin@gmail.com>
2021-08-03 20:33:23 -0500

    add files

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Auth class"""
+
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth():
+    """Auth class to manage
+    the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path"""
+        if path is None or excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path[-1:] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """returns None - request"""
+        if request is None:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request"""
+        return None
