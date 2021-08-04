c832fd99ac3bcaf768276d27e1dcc8ef93083d2c
duvanjm <duvanjarin@gmail.com>
2021-08-04 16:34:36 -0500

    fix pycodestyle

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,9 @@ import split
 
 class BasicAuth(Auth):
     """class BasicAuth that inherits from Auth"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header:
+                                            str) -> str:
         """returns the Base64 part of
         the Authorization header for
         a Basic Authentication"""
@@ -20,7 +22,9 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
         """returns the decoded value of
         a Base64 string base64_authorization_header"""
         if base64_authorization_header is None:
@@ -31,10 +35,12 @@ class BasicAuth(Auth):
             message = base64_authorization_header.encode('utf-8')
             dcode = base64.b64decode(message)
             return dcode.decode('utf-8')
-        except:
+        except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
         """returns the user email and
         password from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
