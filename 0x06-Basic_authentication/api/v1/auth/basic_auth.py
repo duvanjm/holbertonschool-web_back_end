e731e0907353275c9880af591da553159879b6e2
duvanjm <duvanjarin@gmail.com>
2021-08-05 11:21:58 -0500

    fix advanced task

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        data = decoded_base64_authorization_header.split(':')
+        data = decoded_base64_authorization_header.split(':', 1)
         return data[0], data[1]
 
     def user_object_from_credentials(self,
