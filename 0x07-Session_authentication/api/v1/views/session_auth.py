29718d5c5570d24ace8dba4ed6197e6ad6891455
duvanjm <duvanjarin@gmail.com>
2021-08-11 11:39:00 -0500

    add task

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -9,7 +9,38 @@ import os
 from models.user import User
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """handles all routes for the Session authentication"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or len(email) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if password is None or len(password) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        search = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not search:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in search:
+        if user.is_valid_password(password) is True:
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            cookie = os.getenv('SESSION_NAME')
+            responce = jsonify(user.to_json())
+            responce.set_cookie(cookie, session_id)
+            return responce
+        else:
+            return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
     """deletes the user session / logout"""
     from api.v1.app import auth
