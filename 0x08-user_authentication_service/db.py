f5daef608454c5cb60285539c98420059290e536
duvanjm <duvanjarin@gmail.com>
2021-08-15 15:33:30 -0500

    fix

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-"""DB module for manage db
+"""DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
 
 
 class DB:
-    """DB class for manage db
+    """DB class
     """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -34,8 +32,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """returns a User object save the user to the database.
-        """
+        '''returns a User object that should save the user to the database.
+        '''
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
