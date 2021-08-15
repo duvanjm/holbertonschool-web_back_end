64e72b27865ebaf12aa1b8aeaac38b0846c250f7
duvanjm <duvanjarin@gmail.com>
2021-08-15 15:00:59 -0500

    fix

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -3,7 +3,6 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.sql.expression import null
 
 
 Base = declarative_base()
@@ -16,5 +15,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
