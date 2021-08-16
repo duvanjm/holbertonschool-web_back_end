f4f71a9fa80fbf11fb597304d72b27370f698b8b
duvanjm <duvanjarin@gmail.com>
2021-08-16 11:24:04 -0500

    add task

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 """set up a basic Flask app"""
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def message() -> str:
     """return a JSON payload"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    """end-point to register a user."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
