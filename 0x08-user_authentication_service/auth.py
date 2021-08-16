435b25a33f9881bc0b0c8162cc80e386c5cc4d42
duvanjm <duvanjarin@gmail.com>
2021-08-16 11:55:19 -0500

    add task

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -23,9 +24,25 @@ class Auth:
             hash_pwd = _hash_password(password)
             return self._db.add_user(email, hash_pwd)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Try locating the user. If it exists, check the password
+        If it matches return True. Otherwise, return False."""
+        try:
+            find = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), find.hashed_password):
+                return True
+        except NoResultFound:
+            return False
+        return False
+
 
 def _hash_password(password: str) -> str:
     """returned bytes is a
     salted hash of the input password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def _generate_uuid():
+    """return a string representation of a new UUID"""
+    return uuid.uuid4()
