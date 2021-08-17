3005ca723f1d2f0ab9cdc126e3234ba13566df07
duvanjm <duvanjarin@gmail.com>
2021-08-17 10:25:49 -0500

    add task

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -55,14 +55,26 @@ def logout():
 
 @app.route('/profile')
 def profile():
-    """User profile
-    if user exists retunr 200 http status"""
+    """if user exists retunr 200 http status"""
     session_id = request.cookies.get('session_id')
     if session_id is not None:
         user = AUTH.get_user_from_session_id(session_id)
         if user is not None:
             return jsonify({"email": user.email})
     return abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def reset_pwd():
+    """respond to the POST /reset_password route."""
+    email = request.form.get('email')
+    try:
+        find = AUTH._db.find_user_by(email=email)
+        if find:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": token})
+    except NoResultFound:
+        return None
 
 
 if __name__ == "__main__":
