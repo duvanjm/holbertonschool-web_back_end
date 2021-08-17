34cae5b373a2467c95b20bdee01531ff32ab041b
duvanjm <duvanjarin@gmail.com>
2021-08-17 14:07:50 -0500

    fix

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -25,7 +25,6 @@ def register() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """create a new session for the user"""
