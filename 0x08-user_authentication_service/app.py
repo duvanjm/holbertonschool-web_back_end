b2aa8ccb967c8b75ed2d37bb4fc9e52c764c6cc5
duvanjm <duvanjarin@gmail.com>
2021-08-18 10:53:39 -0500

    fix

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -78,7 +78,7 @@ def reset_pwd():
             token = AUTH.get_reset_password_token(email)
             return jsonify({"email": email, "reset_token": token}), 200
     except NoResultFound:
-        return None
+        abort(403)
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
