0f2c77e67163495a3485703c147c889e2029393a
duvanjm <duvanjarin@gmail.com>
2021-08-31 09:44:21 -0500

    add files

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis """
+
+from redis.client import Redis
+from uuid import uuid4
+
+
+class Cache():
+    """Writing strings to Redis """
+
+    def __init__(self):
+        """init method"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """generate a random key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
