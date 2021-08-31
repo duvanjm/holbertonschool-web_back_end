8bfc17fd3ef36b18ad16dd16940ebb83a9184526
duvanjm <duvanjarin@gmail.com>
2021-08-31 10:31:27 -0500

    add task

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis """
 
 import redis
-from typing import Union
+from typing import Union, Callable
 from uuid import uuid4
 
 
@@ -19,3 +19,20 @@ class Cache():
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Callable:
+        """convert the data back to the desired format"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, data: str) -> str:
+        """ parametrize Cache.get with
+        the correct conversion function"""
+        return str(data.encode('utf-8'))
+
+    def get_int(self, number: int) -> int:
+        """ parametrize Cache.get with
+        the correct conversion function"""
+        return int(number)
+
