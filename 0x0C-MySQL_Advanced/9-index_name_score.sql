6e2f75038884231772253146ac4aad1e390ac472
duvanjm <duvanjarin@gmail.com>
2021-09-02 15:28:12 -0500

    add task

--- 0x0C-MySQL_Advanced/9-index_name_score.sql
+++ 0x0C-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- index idx_name_first_score on the
 -- table names and the first letter of name and the score
 
-CREATE INDEX idx_name_first_score ON names(name(1), score(1));
+CREATE INDEX idx_name_first_score ON names(name(1), score);
