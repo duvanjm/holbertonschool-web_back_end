7efb048eb8d2d3c28e09579e8bf9669b28aa34a4
duvanjm <duvanjarin@gmail.com>
2021-09-02 11:47:33 -0500

    add task

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,12 @@
+-- stored procedure AddBonus
+-- that adds a new correction for a student
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
+BEGIN
+    INSERT INTO corrections(user_id, project_name, score) VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
+
+END $$
+
+DELIMITER ;
