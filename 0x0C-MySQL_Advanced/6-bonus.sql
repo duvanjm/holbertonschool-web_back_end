079582b63f6990e39286a7e23291105060a04f8d
duvanjm <duvanjarin@gmail.com>
2021-09-02 13:17:27 -0500

    add task

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -5,8 +5,7 @@ DELIMITER $$
 
 CREATE PROCEDURE AddBonus(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
 BEGIN
-    INSERT INTO corrections(user_id, project_name, score) VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
-
+    INSERT INTO corrections(user_id, project_id, score) VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END $$
 
 DELIMITER ;
