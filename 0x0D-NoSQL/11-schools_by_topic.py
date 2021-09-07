ecf3c4910da2f3f4db798a9a4d99a52d3ae3865f
duvanjm <duvanjarin@gmail.com>
2021-09-07 11:40:13 -0500

    add task

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Where can I learn Python? """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list
+    of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
