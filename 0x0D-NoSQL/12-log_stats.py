d06530dba2b300565216bd381faa0e2ff2725873
duvanjm <duvanjarin@gmail.com>
2021-09-07 12:01:41 -0500

    add task

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Log stats """
+
+from pymongo import MongoClient
+
+
+client = MongoClient()
+
+col = client.logs.nginx
+
+logs = col.count_documents({})
+get = col.count_documents({"method": "GET"})
+post = col.count_documents({"method": "POST"})
+put = col.count_documents({"method": "PUT"})
+patch = col.count_documents({"method": "PATCH"})
+delete = col.count_documents({"method": "DELETE"})
+status = col.count_documents({"method": "GET", "path": "/status"})
+
+
+if __name__ == '__name__':
+    print(f"{logs} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{status} status check")
+
