79b4c3657d2879faa9b99b16cd45f2dcfaf6988d
duvanjm <duvanjarin@gmail.com>
2021-09-07 11:35:17 -0500

    add task

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Change school topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all
+    topics of a school document"""
+    mongo_collection.update_many({"name": name},
+                                 {"$set":
+                                 {"topic": topics}})
