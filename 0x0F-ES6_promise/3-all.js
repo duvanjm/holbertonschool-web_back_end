7dffbfb9b207bab41c8fadce5ac4e1d695b7b919
duvanjm <duvanjarin@gmail.com>
2021-09-14 09:42:44 -0500

    fix

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -7,5 +7,5 @@ export default function handleProfileSignup() {
   return Promise.all([promise1, promise2]).then((values) => {
     console.log(values[0].body, values[1].firstName, values[1].lastName);
   })
-  .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
