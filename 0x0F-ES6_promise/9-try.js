c3735f16e4aa30b55786f44e27c2431ce55cdae9
duvanjm <duvanjarin@gmail.com>
2021-09-14 12:13:49 -0500

    fix

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,9 +1,8 @@
 const guardrail = (mathFunction) => {
   const queue = [];
-  const result = mathFunction();
   try {
-  queue.push(result);
-  } catch (error){
+    queue.push(mathFunction());
+  } catch (error) {
     queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
