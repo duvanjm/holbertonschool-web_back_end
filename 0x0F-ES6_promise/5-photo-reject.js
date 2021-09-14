42544d653555e6b0cc5294825618a2a2562a16c7
duvanjm <duvanjarin@gmail.com>
2021-09-14 10:16:50 -0500

    fix reject

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+  return new Promise(() => {
+    throw `${filename} cannot be processed`;
   });
 }
