acbc9c56aebc1b9fb890f8204d5a5c603d2b1da3
duvanjm <duvanjarin@gmail.com>
2021-09-16 13:27:44 -0500

    add task

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -6,14 +6,6 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(val) {
-    this._sqft = val;
-  }
-
   get floors() {
     return this._floors;
   }
