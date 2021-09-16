955e925a26cdef3edbcbedec326ec5edd9c8dd05
duvanjm <duvanjarin@gmail.com>
2021-09-16 13:19:47 -0500

    add task

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
