8518ea7d4e6a331bfa62fcccf0be02da180e9abe
duvanjm <duvanjarin@gmail.com>
2021-09-16 13:15:49 -0500

    add task

---
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const lst = [];
+
+  lst.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34));
+
+  return lst;
+}
