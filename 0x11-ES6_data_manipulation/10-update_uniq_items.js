4978e755d953db9f77eafd93470301c6ae4b1819
duvanjm <duvanjarin@gmail.com>
2021-09-21 12:39:54 -0500

    fix throw error

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,11 @@
 export default function updateUniqueItems(map) {
+  if (map instanceof Map === false) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, val] of map) {
     if (val === 1) {
       map.set(key, 100);
     }
   }
-  return map;
 }
