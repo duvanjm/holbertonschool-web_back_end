7a13ea3ca416da3bc64525856b18ea0d7c42a24d
duvanjm <duvanjarin@gmail.com>
2021-09-21 12:53:32 -0500

    add task

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    if (weakMap.get(endpoint) > 5) {
+      throw new Error('Endpoint load is high');
+    }
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
