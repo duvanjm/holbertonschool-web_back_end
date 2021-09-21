8694180f1235a5f07d52214d9d17cb5480b46b7a
duvanjm <duvanjarin@gmail.com>
2021-09-21 12:55:44 -0500

    fix

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-    if (weakMap.get(endpoint) > 5) {
+    if (weakMap.get(endpoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
   } else {
