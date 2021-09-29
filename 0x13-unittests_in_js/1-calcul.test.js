e2eae2b3fe57b239ff6f3c1dcc88f111dc535152
duvanjm <duvanjarin@gmail.com>
2021-09-29 12:13:29 -0500

    add more test

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -4,16 +4,24 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
   it('return Error', () => {
-    assert.equal(calculateNumber('DIVIDE', 1, 0), 'Error');
+    assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
   });
   it('return correct value sum', () => {
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM',1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM',1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM',1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber('SUM',-1.5, -3.7), -5);
+    assert.strictEqual(calculateNumber('SUM',0.4, 0.5), 1);
   });
   it('return correct value sub', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.8), -3);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
   });
   it('return correct value div', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
-  
+
 });
