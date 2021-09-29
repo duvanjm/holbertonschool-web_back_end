a6e4f752f0bc6e510e7382a6dba0689af14cf9e2
duvanjm <duvanjarin@gmail.com>
2021-09-29 12:23:18 -0500

    add more test

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -13,15 +13,18 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber('SUM',1.5, 3.7), 6);
     assert.strictEqual(calculateNumber('SUM',-1.5, -3.7), -5);
     assert.strictEqual(calculateNumber('SUM',0.4, 0.5), 1);
+    assert.strictEqual(calculateNumber('SUM', 0.5), NaN);
   });
   it('return correct value sub', () => {
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.8), -3);
     assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
     assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
+    assert.strictEqual(calculateNumber('SUBTRACT', 0.5), NaN);
   });
   it('return correct value div', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 0.5), NaN);
   });
   it('return error', () => {
     assert.strictEqual(calculateNumber('x', 3, 5), undefined);
