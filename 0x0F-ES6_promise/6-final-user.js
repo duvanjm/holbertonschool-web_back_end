813c7ddfc08c10770158e5cfc440b2e02d92841a
duvanjm <duvanjarin@gmail.com>
2022-06-06 08:51:12 -0500

    add

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,8 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
-    console.log(values);
-  })
-  .catch((err) => console.log(err));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    // eslint-disable-next-line consistent-return
+    .then((values) => {
+      if (values[0] === 'fulfilled') {
+        return values;
+      }
+    })
+    // eslint-disable-next-line no-console
+    .catch((err) => console.log(err));
 }
